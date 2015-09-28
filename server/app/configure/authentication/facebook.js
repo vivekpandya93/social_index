e8ec77405af025cea9fb96aff6197bcc922c3b1c
Vivek Pandya <vivek.pandya93@gmail.com>
2015-09-28 08:54:30 -0400

    stuff

---
+++ server/app/configure/authentication/facebook.js
@@ -0,0 +1,54 @@
+'use strict';
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var mongoose = require('mongoose');
+var UserModel = mongoose.model('User');
+
+module.exports = function (app) {
+
+    var facebookConfig = app.getValue('env').FACEBOOK;
+
+    var facebookCredentials = {
+        clientID: facebookConfig.clientID,
+        clientSecret: facebookConfig.clientSecret,
+        callbackURL: facebookConfig.callbackURL
+    };
+
+    var verifyCallback = function (accessToken, refreshToken, profile, done) {
+
+        UserModel.findOne({ 'facebook.id': profile.id }).exec()
+            .then(function (user) {
+
+                if (user) {
+                    return user;
+                } else {
+                    return UserModel.create({
+                        facebook: {
+                            id: profile.id
+                        }
+                    });
+                }
+
+            }).then(function (userToLogin) {
+                done(null, userToLogin);
+            }, function (err) {
+                console.error('Error creating user from Facebook authentication', err);
+                done(err);
+            })
+
+    };
+
+    passport.use(new FacebookStrategy(facebookCredentials, verifyCallback));
+
+    app.get('/auth/facebook', passport.authenticate('facebook'));
+
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', { failureRedirect: '/login' }),
+        function (req, res) {
+            res.redirect('/');
+        });
+    // app.get('/oauth/access_token?client_id=facebookCredentials.clientID&client_secret=facebookCredentials.client_secret&grant_type=client_credentials', function(request, response) {
+    //     console.log("FB says", response)
+    // });
+
+};
---
+++ server/env/development.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
+  "SESSION_SECRET": "Optimus Prime is my real dad",
+  "TWITTER": {
+    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
+    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
+    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
+  },
+  "FACEBOOK": {
+    "clientID": "546034522214538",
+    "clientSecret": "c4299b9b5e69964741ad18c1e698aafb",
+    "callbackURL": "http://127.0.0.1:1337/auth/facebook/callback"
+  },
+  "GOOGLE": {
+    "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
+    "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
+    "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
+  }
+};
